ab3d35feb8a80131e795788f5f34bb813fcfe5c2
yeonkwon <46590610+yeonkwon@users.noreply.github.com>
2025-09-03 09:58:58 +0900

    Update vite.config.ts
    add base (/repo-name/)

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/dopamin-rush-play/'
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
